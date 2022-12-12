71e620ed6275b5805f8bbf241943bf0113ff30e8
visheshpuri1708 <95050304+visheshpuri1708@users.noreply.github.com>
2022-12-12 07:53:56 -0330

    Update Doctor.js

--- client/src/components/Doctor.js
+++ client/src/components/Doctor.js
@@ -5,7 +5,7 @@ function Doctor({ doctor }) {
     const navigate = useNavigate();
     return (
         <div
-            className="card p-2 cursor-pointer"
+            className="card p-2 cursor-pointer mb-3"
             onClick={() => navigate(`/book-appointment/${doctor._id}`)}
         >
             <h1 className="card-title">
@@ -21,12 +21,16 @@ function Doctor({ doctor }) {
                 {doctor.address}
             </p>
             <p>
-                <b>Fee per Visit : </b>
+                <b>Fee per Visit : $</b>
                 {doctor.feePerCunsultation}
             </p>
             <p>
                 <b>Timings : </b>
                 {doctor.timings[0]} - {doctor.timings[1]}
+            </p>
+            <p>
+                <b>Specialization : </b>
+                {doctor.specialization}
             </p>
         </div>
     )
